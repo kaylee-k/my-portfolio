eafec1ca3e19cde6ba282cdcbc664a1b379504fe
kaylee <151891463+kaylee-k@users.noreply.github.com>
2023-12-16 22:51:43 -0700

    JS - Home

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 // Header에 페이지 아래로 스크롤시 다크 스타일링 적용
 const header = document.querySelector('.header');
-const headerHeight = header.getBoundingClientRect().height;
+const headerHeight = header.offsetHeight;
 document.addEventListener('scroll', () => {
     console.log(window.scrollY); 
     if (window.scrollY > headerHeight) {
@@ -10,3 +10,10 @@ document.addEventListener('scroll', () => {
     }
 });
 
+//  Home 섹션을 아래로 스크롤시 투명하게 처리함
+const home = document.querySelector('.home__container');
+const homeHeight = home.offsetHeight;
+document.addEventListener('scroll', () => {
+    console.log(1 - window.scrollY / homeHeight);
+    home.style.opacity = 1 - window.scrollY / homeHeight;
+});
