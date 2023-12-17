3e3a1331a7768b51782164dd62648c46e46abef0
kaylee <151891463+kaylee-k@users.noreply.github.com>
2023-12-16 23:03:38 -0700

    JS - ArrowUp

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@
 const header = document.querySelector('.header');
 const headerHeight = header.offsetHeight;
 document.addEventListener('scroll', () => {
-    console.log(window.scrollY); 
     if (window.scrollY > headerHeight) {
         header.classList.add('header--dark');
     } else {
@@ -14,6 +13,16 @@ document.addEventListener('scroll', () => {
 const home = document.querySelector('.home__container');
 const homeHeight = home.offsetHeight;
 document.addEventListener('scroll', () => {
-    console.log(1 - window.scrollY / homeHeight);
     home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
+// Arrow up 버튼을 아래로 스크롤시 투명하게 처리한다
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll', () => {
+    if(window.scrollY > homeHeight / 2) {
+     arrowUp.style.opacity = 1;
+    }
+    else {
+     arrowUp.style.opacity = 0;
+    }
 });
