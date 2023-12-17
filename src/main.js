87d8d466566ef82314a9eed2512805ea2b8fbf8d
kaylee <151891463+kaylee-k@users.noreply.github.com>
2023-12-16 23:46:16 -0700

    JS - Menu

--- src/main.js
+++ src/main.js
@@ -26,3 +26,15 @@ document.addEventListener('scroll', () => {
      arrowUp.style.opacity = 0;
     }
 });
+
+// Navbar 토글버튼 클릭 처리
+const navbarMenu = document.querySelector('.header__menu');
+const navbarToggle = document.querySelector('.header__toggle');
+navbarToggle.addEventListener('click', () => {
+    navbarMenu.classList.toggle('open');
+});
+
+// Navbar 메뉴 클릭시 메뉴를 자동으로 닫아줌
+navbarMenu.addEventListener('click', () => {
+    navbarMenu.classList.remove('open');
+});
