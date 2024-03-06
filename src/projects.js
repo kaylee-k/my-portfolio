893399dfec6445d0e1880d39548df12ad6c16364
kaylee <151891463+kaylee-k@users.noreply.github.com>
2024-03-06 14:54:07 -0700

    Overall revision

--- src/projects.js
+++ src/projects.js
@@ -1,42 +1,37 @@
 'use strict';
 
-// 프로젝트 필터링 관련 로직 처리
 const categories = document.querySelector('.categories');
 const projects = document.querySelectorAll('.project');
 const projectsContainer = document.querySelector('.projects');
-categories.addEventListener('click', (event)=> {
-    const filter = event.target.dataset.category;
-    // 사각형사이의 빈공간을 콘솔창찍어보면 null이 나오는데
-    // 그걸 없애주기위함. return의 의미는 종료
-     if(filter == null) {
-        return;
-   }
+categories.addEventListener('click', (event) => {
+  const filter = event.target.dataset.category;
 
-   handleActiveSelection(event.target);
-   filterProjects(filter);
+  if (filter == null) {
+    return;
+  }
+
+  handleActiveSelection(event.target);
+  filterProjects(filter);
 });
 
-// 함수이름은 동사로 시작하기
-function handleActiveSelection(target){
-      const active = document.querySelector('.category--selected');
-      // classList는 그자체로 클래스라는 의미를 내포하고
-      //  있기때문에 '.category'가 아니라 쩜을 생략해야함
-      active.classList.remove('category--selected');
-      target.classList.add('category--selected');
+function handleActiveSelection(target) {
+  const active = document.querySelector('.category--selected');
+
+  active.classList.remove('category--selected');
+  target.classList.add('category--selected');
 }
 
-// 함수이름은 동사로 시작하기
 function filterProjects(filter) {
-   projects.forEach((project) => {
+  projects.forEach((project) => {
     console.log(project.dataset.type);
-    if(filter === 'all' || filter === project.dataset.type){
-        project.style.display = 'block';
+    if (filter === 'all' || filter === project.dataset.type) {
+      project.style.display = 'block';
     } else {
-        project.style.display = 'none';
+      project.style.display = 'none';
     }
-   });
-    projectsContainer.classList.add('anim-out');
-   setTimeout(()=>{
+  });
+  projectsContainer.classList.add('anim-out');
+  setTimeout(() => {
     projectsContainer.classList.remove('anim-out');
-   }, 250);
+  }, 250);
 }
